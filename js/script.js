c1a4b2c1a7b841e1e4672f7757047ff1acb6fa7d
Romashka <rroma13579@gmail.com>
2022-08-11 23:25:26 +0200

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,76 +1,99 @@
-// window.confirm("Are you ready to play?")
-// window.confirm("The rules of the game are the following: you have to write three numers and then you will see a pannel width 9 symbols, three of them are numbers from 1 to 3, the place of numbers depends on your numbers. Oter symbels are 'X'")
-// while(window.confirm("Are you ready to play?")){
-//     var conf,n
-//     var numbers = []
-//     conf = prompt("Choose 3 different numbers from 1 to 9")
-//     if(conf!=null){
-//         for(var k = 1; k<=9; k++){
-//             if(conf.indexOf(k) != -1){
-//                 numbers[k-1] = Math.ceil(3*Math.random())
-//             }
-//             else{
-//                 numbers[k-1] = "X"
-//             }
-//         }
-//         for(var n = 1; n<=9; n++){
-//             if(n%3 == 0){
-//                 document.write(" "+numbers[n-1]+"<br>")
-//             }
-//             else{
-//                 document.write(" "+numbers[n-1]+" ")
-//             }
-//         }
-//         var counter1 = 0
-//         var counter2 = 0
-//         var counter3 = 0
-//         var counter4 = 0
-//         for(var f = 1; f<=9; f++){
-//             if(numbers[f-1] == "1"){
-//                 counter1++
-//             }
-//             else if(numbers[f-1] == "2"){
-//                 counter2++
-//             }
-//             else if(numbers[f-1] == "3"){
-//                 counter3++
-//             }
-//             else{
-//                 counter4++
-//             }
-//         }
-//         if(counter4 < 6){
-//             document.write("<h4>YOU ARE CHEATING</h4>")
-//         }
-//         else if(counter1 == 3 || counter2 == 3 || counter3 == 3){
-//             document.write("<h4>YOU WIN</h4>")
-//         }
-//         else if(counter1 == 2 || counter2 == 2 || counter3 == 2){
-//             document.write("<h5>You have 2 similar nubers/Draw</h5>")
-//         }
-//         else{
-//             document.write("<h5>You lose</h5>")
-//         }
-//         document.write("<br>")
-//     }
-//     else{
-//         break
-//     }
-// }
-// document.write("END")
+var choose = prompt("What you want: 1. A game with numbers; 2. Try-Catch test; 3. Timer in console")
+switch (choose) {
+    case "1":
+    case "game":
+    case "A game with numbers":
+        game()
+        break
+    case "2":
+    case "Try-Catch":
+    case "Try-Catch test":
+        try_catch()
+        break
+    case "3":
+    case "Timer":
+    case "Timer in console":
+        timer()
+        break
+}
 
-// window.confirm("Try-catch")
-// try{
-//     var number1, number2, sum
-//     number1 = prompt("Type first number")
-//     number2 = prompt("Type second number")
-//     sum = (number1+" + "+number2)
-//     document.write(""+sum+" = "+eval(sum)+" ")
-// }
-// catch{
-//     document.write("ERROR")
-// }
-// document.write("<br>END")
+function game() {
+    window.confirm("Are you ready to play?")
+    window.confirm("The rules of the game are the following: you have to write three numers and then you will see a pannel width 9 symbols, three of them are numbers from 1 to 3, the place of numbers depends on your numbers. Oter symbels are 'X'")
+    while (window.confirm("Are you ready to play?")) {
+        var conf, n
+        var numbers = []
+        conf = prompt("Choose 3 different numbers from 1 to 9")
+        if (conf != null) {
+            for (var k = 1; k <= 9; k++) {
+                if (conf.indexOf(k) != -1) {
+                    numbers[k - 1] = Math.ceil(3 * Math.random())
+                }
+                else {
+                    numbers[k - 1] = "X"
+                }
+            }
+            for (var n = 1; n <= 9; n++) {
+                if (n % 3 == 0) {
+                    document.write(" " + numbers[n - 1] + "<br>")
+                }
+                else {
+                    document.write(" " + numbers[n - 1] + " ")
+                }
+            }
+            var counter1 = 0
+            var counter2 = 0
+            var counter3 = 0
+            var counter4 = 0
+            for (var f = 1; f <= 9; f++) {
+                if (numbers[f - 1] == "1") {
+                    counter1++
+                }
+                else if (numbers[f - 1] == "2") {
+                    counter2++
+                }
+                else if (numbers[f - 1] == "3") {
+                    counter3++
+                }
+                else {
+                    counter4++
+                }
+            }
+            if (counter4 < 6) {
+                document.write("<h4>YOU ARE CHEATING</h4>")
+            }
+            else if (counter1 == 3 || counter2 == 3 || counter3 == 3) {
+                document.write("<h4>YOU WIN</h4>")
+            }
+            else if (counter1 == 2 || counter2 == 2 || counter3 == 2) {
+                document.write("<h5>You have 2 similar nubers/Draw</h5>")
+            }
+            else {
+                document.write("<h5>You lose</h5>")
+            }
+            document.write("<br>")
+        }
+        else {
+            break
+        }
+    }
+    document.write("END")
+}
+
+function try_catch() {
+    window.confirm("Try-catch")
+    try {
+        var number1, number2, sum
+        number1 = prompt("Type first number")
+        number2 = prompt("Type second number")
+        sum = (number1 + " + " + number2)
+        document.write("" + sum + " = " + eval(sum) + " ")
+    }
+    catch {
+        document.write("ERROR")
+    }
+    document.write("<br>END")
+}
 
 // var numbers = []
 // var number = prompt("Type number")
@@ -119,34 +142,36 @@
 // document.write("- " + numbers[k] + " ")
 // document.write("<br>END")
 
-document.write("Timer in console")
-var seconds = 0
-var minutes = 0
-var hours = 0
-function Seconds() {
-    seconds++
-    if (seconds == "60") {
-        seconds -= 60
-        Minutes()
+function timer() {
+    document.write("Timer in console")
+    var seconds = 0
+    var minutes = 0
+    var hours = 0
+    function Seconds() {
+        seconds++
+        if (seconds == "60") {
+            seconds -= 60
+            Minutes()
+        }
+        console.log("" + hours + " " + minutes + " " + seconds + "")
     }
-    console.log(""+hours+" "+minutes+" "+seconds+"")
-}
-function Minutes() {
-    minutes++
-    if (minutes == "60") {
-        minutes -= 60
-        Hours()
+    function Minutes() {
+        minutes++
+        if (minutes == "60") {
+            minutes -= 60
+            Hours()
+        }
     }
-}
-function Hours() {
-    hours++
-    if (hours == "24") {
-        hours -= 24
-        Text_f()
+    function Hours() {
+        hours++
+        if (hours == "24") {
+            hours -= 24
+            Text_f()
+        }
     }
+    function Text_f() {
+        document.write("Day has passed")
+        clearInterval()
+    }
+    setInterval(Seconds, 1000)
 }
-function Text_f() {
-    document.write("Day has passed")
-    clearInterval()
-}
-setInterval(Seconds, 1000)
